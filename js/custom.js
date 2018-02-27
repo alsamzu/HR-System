94e0480cd54f11f1bd733d4dba8f00de8ed91e50
alsamzu <zulfalsam@gmail.com>
2018-02-27 21:09:43 +0300

    finance manager login

--- js/custom.js
+++ js/custom.js
@@ -3,19 +3,35 @@
 function login(){
     var b=alert("Welcome to xyz site.Please click ok to proceed");
     var a = prompt("Kindly choose your user name to proceed \n 1)Admin \n 2)Finance Manager \n 3)User");
-    if(a=1){
+    if(a==1){
        var c= prompt("Welcome admin. Please put your password to proceed");
         if(c==54321){
-            alert("Login was succesful");
+            alert("Admin your login was succesful");
             
         }
-        else if (c = null){
+        else if (c.length <=0){
             alert("Password can not be null");
         }
-        else{
-            alert("Incorrect Password");
-        }
+       
+        else if (c!=54321){
+           alert("Incorrect Password for admin account");
+      }
+      
         
     }
+    else if( a==2){
+        var d=prompt("Welcome Finance Manager.Please put your password to proceed");
+        if(d==12345){
+            alert("Finance Manager your login was succesful");
+        }
+        else if (d.length <=0){
+            alert("Password can not be null");
+        }
+       
+        else if (d!=12345){
+           alert("Incorrect Password for finance account");
+      }
+    }
+   
 }
 login();
