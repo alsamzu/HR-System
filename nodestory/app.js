bff3a96b55a07d7d9f05bd7fbf509ed93e2bef18
alsamzu <zulfalsam@gmail.com>
2018-05-17 10:22:10 +0300

    module patterns

--- nodestory/app.js
+++ nodestory/app.js
@@ -1,3 +1,4 @@
 var stuff= require('./stuff');
-console.log(stuff.counter(['zulfa',"Ann","Ali",22]));
-console.log(stuff.adder(5,7));
+//console.log(stuff.counter(['zulfa',"Ann","Ali",22]));
+//console.log(stuff.adder(5,7));
+console.log(stuff.pi(55,7));
--- nodestory/stuff.js
+++ nodestory/stuff.js
@@ -1,7 +1,14 @@
-module.exports.counter = function(arr){
-    return "There are "+arr.length+" elements in this array";
-};
-
-module.exports.adder = function(a,b){
-    return `The sum of the 2 numbers is ${a+b}`;
+//module.exports.counter = function(arr){
+//    return "There are "+arr.length+" elements in this array";
+//};
+//
+//module.exports.adder = function(a,b){
+//    return `The sum of the 2 numbers is ${a+b}`;
+//}
+//another module pattern to export our data
+var pi = function(c,d){
+    return`pi is ${c-d}`;
 }
+module.exports={
+    pi:pi
+};
