656b5e2fd79f209c99993e19c941e177a66a59d4
alsamzu <zulfalsam@gmail.com>
2018-05-28 12:26:31 +0300

    Write on file

---
+++ nodestory/ReadWriteFiles.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+//Method to read files
+//It is  synchronuos method it blocks any code below it until it is executed fully
+var readMe = fs.readFileSync('readme.txt','utf8');
+console.log(readMe);
+
+//Writing into a file
+var writeMe = fs.writeFileSync('writeme.txt',readMe +'Yeeeey');
+
+//Asynchronous read file
+//err for any error
+fs.readFile('readme.txt','utf8', function(err,data){
+    console.log(data);
+});
