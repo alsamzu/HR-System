53410c1db1c66b7b33b7ebdd5dff67c1c9260525
alsamzu <zulfalsam@gmail.com>
2018-05-28 12:50:39 +0300

    Creating a dir Async

--- nodestory/mkdir.js
+++ nodestory/mkdir.js
@@ -2,4 +2,16 @@ var fs = require('fs');
 // SYNC METHOD FOR CREATING A DIRECTORY
 //fs.mkdirSync('newDir');
 //REMOVING DIR
-fs.rmdirSync('newDir');
+//fs.rmdirSync('newDir');
+
+//ASYNC METHOD
+//fs.mkdir('newDir',function(){
+//fs.readFile('readme.txt','utf8',function(err, data){
+//    fs.writeFile('./newDir/writeMe.txt',data);
+//});
+//});
+
+//DELETING DIR ASYNC
+//We cant remove a dir if it is not empty
+//we first remove the files inside
+fs.
