2e07d9557c5fc8e65819c52431aef1fc7be91788
alsamzu <zulfalsam@gmail.com>
2018-05-28 11:48:57 +0300

    Event Example

--- nodestory/event.js
+++ nodestory/event.js
@@ -1,1 +1,9 @@
 var events = require('events');
+//Our own Events
+var myEmitter = new events.EventEmitter();
+//Do something when the event occurs
+myEmitter.on('someEvent',function(mssg){
+             console.log(mssg);
+             });
+
+myEmitter.emit('someEvent',"The event was emitted");
