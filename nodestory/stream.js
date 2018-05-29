49c4fcea3d5fe7b0033a064f68a12450049c3215
alsamzu <zulfalsam@gmail.com>
2018-05-29 10:01:06 +0300

    writable Stream

--- nodestory/stream.js
+++ nodestory/stream.js
@@ -1,4 +1,16 @@
 //CREATING A READ STREAM
 var http = require('http');
 var fs = require('fs');
-var myReadStream = fs.createReadStream(_dirname +'/readme.txt');
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+//listing for an event
+//myReadStream.on('data',function(chunk){
+//    console.log('new chunk received: ');
+//    console.log(chunk);
+});
+
+//CREATING A WRITABLE STREAM
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt','utf8');
+myReadStream.on('data',function(chunk){
+    console.log('new chunk received: ');
+        myWriteStream.write(chunk);
+});
