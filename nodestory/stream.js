330376c8ab29c9786b41ff592cad420407bf33ec
alsamzu <zulfalsam@gmail.com>
2018-05-29 10:08:36 +0300

    Pipes

--- nodestory/stream.js
+++ nodestory/stream.js
@@ -6,11 +6,15 @@ var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
 //myReadStream.on('data',function(chunk){
 //    console.log('new chunk received: ');
 //    console.log(chunk);
-});
+//}
+);
 
 //CREATING A WRITABLE STREAM
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt','utf8');
-myReadStream.on('data',function(chunk){
-    console.log('new chunk received: ');
-        myWriteStream.write(chunk);
-});
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+//myReadStream.on('data',function(chunk){
+//    console.log('new chunk received: ');
+//        myWriteStream.write(chunk);
+//});
+
+//CREATING PIPES
+myReadStream.pipe(myWriteStream);
