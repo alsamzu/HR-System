81f4abc51872f922d2b05225d74692e2c9773276
alsamzu <zulfalsam@gmail.com>
2018-05-29 11:56:57 +0300

    Serving JSON

--- nodestory/bunnyAsync.js
+++ nodestory/bunnyAsync.js
@@ -19,3 +19,17 @@ processing. Thus you won't see the change taking place until you call
 'node bunnyAsync.js' in your terminal again.
 */
 fs.appendFileSync('wishlist.html', '6. Shout "Happy with Async!!"', 'utf8');
+
+//SYNC METHOD
+// blocking / sync code:
+// you have to require the file system(fs), Node.js's core module for it to work:
+//var fs = require('fs');
+//
+//// First read the file 'wishlist.html' synchronously using the readFileSync method:
+//// We have to use the toString because it returns as a buffer.
+//// You can learn about buffers in the resources I provide at the end of this article.
+//var wishlist = fs.readFileSync('wishlist.html').toString();
+////print the file
+//console.log(wishlist);
+//// Do another thing:
+//console.log('ALERT: Pounce owner!!!');
