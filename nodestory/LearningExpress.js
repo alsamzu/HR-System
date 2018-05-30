a74a8a6aa7779fe9a9f060448c8bb8cd034de714
alsamzu <zulfalsam@gmail.com>
2018-05-30 07:24:58 +0300

    Express Params

--- nodestory/LearningExpress.js
+++ nodestory/LearningExpress.js
@@ -14,6 +14,10 @@ app.get('/contact',function(req,res){
     res.send('Welcome to the Contact Page');
 
 });
+app.get('/profile/:id',function(req,res){
+    res.send('You requested to see a profile with the id of '+req.params.id);
+
+});
 
 
 app.listen(3000);
