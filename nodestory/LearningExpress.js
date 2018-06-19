2422df56528f5a775a72deef6faa1e9237322095
alsamzu <zulfalsam@gmail.com>
2018-06-19 18:42:20 +0300

    routing

--- nodestory/LearningExpress.js
+++ nodestory/LearningExpress.js
@@ -56,4 +56,21 @@ app.get('/contact',function(req,res){
     res.render('contact',{qs:req.query});
 
 });
+
+// wiki.js - Wiki route module
+
+var express = require('express');
+var router = express.Router();
+
+// Home page route
+router.get('/', function(req, res) {
+  res.send('Wiki home page');
+});
+
+// About page route
+router.get('/about', function(req, res) {
+  res.send('About this wiki');
+});
+
+module.exports = router;
 app.listen(3000);
