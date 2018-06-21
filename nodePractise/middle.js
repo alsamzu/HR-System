3c8184970435f884f3ac0a0528901ad7fabef878
alsamzu <zulfalsam@gmail.com>
2018-06-21 08:08:45 +0300

    funtion use

--- nodePractise/middle.js
+++ nodePractise/middle.js
@@ -7,3 +7,9 @@ var a_middleware_function = function(req, res, next) {
   next();
     // Call next() so Express will call the next middleware function in the chain.
 }
+
+// Function added with use() for all routes and verbs
+app.use(a_middleware_function);
+
+// Function added with use() for a specific route
+app.use('/someroute', a_middleware_function);
