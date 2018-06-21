e98287c94339325d8b15ef7648e25903be7aa9cf
alsamzu <zulfalsam@gmail.com>
2018-06-21 08:09:50 +0300

    function added for a specific HTTP verb and route

--- nodePractise/middle.js
+++ nodePractise/middle.js
@@ -13,3 +13,8 @@ app.use(a_middleware_function);
 
 // Function added with use() for a specific route
 app.use('/someroute', a_middleware_function);
+
+// A middleware function added for a specific HTTP verb and route
+app.get('/', a_middleware_function);
+
+app.listen(3000);
