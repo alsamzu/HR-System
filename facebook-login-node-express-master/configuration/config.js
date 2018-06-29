8069b4680ce44890eacd80f33c8ea38dac81332c
alsamzu <zulfalsam@gmail.com>
2018-06-29 20:35:33 +0300

    login with facebook

---
+++ facebook-login-node-express-master/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"680393318958343",
+	"facebook_api_secret"	:				"ea57405bddb68d5b599b760bb6aba968",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
